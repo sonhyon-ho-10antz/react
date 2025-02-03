7c1a6e2cf83b9f27214ffaf162bf58db45145574
sonhyon.ho <sonhyon.ho@10antz.co.jp>
2025-02-03 18:50:12 +0900

    js簡略化

--- web2/src/pages/Animation01/Animation01.js
+++ web2/src/pages/Animation01/Animation01.js
@@ -1,17 +1,17 @@
-export default function animateEgg() {
-    const frontEgg = document.querySelector(".front_egg");
-    const backEggTop = document.querySelector(".back_egg_top");
+// export default function animateEgg() {
+//     const frontEgg = document.querySelector(".front_egg");
+//     const backEggTop = document.querySelector(".back_egg_top");
   
-    if (!frontEgg || !backEggTop) return;
+//     if (!frontEgg || !backEggTop) return;
   
-    frontEgg.addEventListener("click", function () {
-      frontEgg.classList.add("break");
-      setTimeout(function () {
-        backEggTop.classList.add("moveUp");
-        setTimeout(function () {
-          frontEgg.classList.remove("break");
-          backEggTop.classList.remove("moveUp");
-        }, 2000);
-      }, 2000);
-    });
-}
+//     frontEgg.addEventListener("click", function () {
+//       frontEgg.classList.add("break");
+//       setTimeout(function () {
+//         backEggTop.classList.add("moveUp");
+//         setTimeout(function () {
+//           frontEgg.classList.remove("break");
+//           backEggTop.classList.remove("moveUp");
+//         }, 2000);
+//       }, 2000);
+//     });
+// }
--- web2/src/pages/Animation01/Animation01_Page.jsx
+++ web2/src/pages/Animation01/Animation01_Page.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Animation01.css";
-import animateEgg from "./Animation01";
 
 export default function Animation01_Page() {
-  React.useEffect(() => {
-    animateEgg();
-  }, []);
+  const [isBroken, setIsBroken] = useState(false);
+  const [isMoving, setIsMoving] = useState(false);
+
+  const animateEggClick = () => {
+    //　アニメ１実行
+    setIsBroken(true);
+    setTimeout(() => {
+      //　アニメ２実行
+      setIsMoving(true);
+      setTimeout(() => {
+        //　リセット
+        setIsBroken(false);
+        setIsMoving(false);
+      }, 2000);
+    }, 2000);
+  };
 
   return (
     <div className="page">
       <h1>Animation01</h1>
       <div className="animation_box">
-          <div className="front_egg"></div>
-          <div className="back_egg">
-              <div className="back_egg_top"></div>
-              <div className="back_egg_bottom"></div>
-          </div>
+        <div className={`front_egg ${isBroken ? "break" : ""}`} onClick={animateEggClick}></div>
+        <div className="back_egg">
+          <div className={`back_egg_top ${isMoving ? "moveUp" : ""}`}></div>
+          <div className="back_egg_bottom"></div>
+        </div>
       </div>
     </div>
   );

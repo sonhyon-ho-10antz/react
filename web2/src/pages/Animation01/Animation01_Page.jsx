eb283a9b24b55f1842a75d560f8c51c36f03b42d
sonhyon.ho <sonhyon.ho@10antz.co.jp>
2025-02-17 16:24:19 +0900

    揺れる動き追加

--- web2/src/pages/Animation01/Animation01_Page.jsx
+++ web2/src/pages/Animation01/Animation01_Page.jsx
@@ -4,17 +4,27 @@ import "./Animation01.css";
 export default function Animation01_Page() {
   const [isBroken, setIsBroken] = useState(false);
   const [isMoving, setIsMoving] = useState(false);
+  const [isShake, setIsShake] = useState(false);
+
 
   const animateClick = () => {
-    //　アニメ１実行
+    // 割れる＋揺れる開始
     setIsBroken(true);
+    setIsShake(true);
+
+    // 揺れ停止
     setTimeout(() => {
-      //　アニメ２実行
-      setIsMoving(true);
+      setIsShake(false);
+
+      // 蓋上昇開始
       setTimeout(() => {
+        setIsMoving(true);
+        
         //　リセット
-        setIsBroken(false);
-        setIsMoving(false);
+        setTimeout(() => {
+          setIsBroken(false);
+          setIsMoving(false);
+        }, 2000);
       }, 2000);
     }, 2000);
   };
@@ -23,10 +33,12 @@ export default function Animation01_Page() {
     <div className="page">
       <h1>Animation01</h1>
       <div className="animation_box">
-        <div className={`front_egg ${isBroken ? "break" : ""}`} onClick={animateClick}></div>
-        <div className="back_egg">
-          <div className={`back_egg_top ${isMoving ? "moveUp" : ""}`}></div>
-          <div className="back_egg_bottom"></div>
+        <div className={`egg ${isShake ? "shake" : ""}`}>
+          <div className={`front_egg ${isBroken ? "break" : ""}`} onClick={animateClick}></div>
+          <div className={`back_egg ${isShake ? "shake" : ""}`}>
+            <div className={`back_egg_top ${isMoving ? "moveUp" : ""}`}></div>
+            <div className="back_egg_bottom"></div>
+          </div>
         </div>
       </div>
     </div>
